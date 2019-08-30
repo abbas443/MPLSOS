b59fedb0570506b20b238cea0f39c4a1fb8a023f
abbas443 <43377782+abbas443@users.noreply.github.com>
2019-08-30 13:44:53 +0100

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,3 +1,3 @@
-
+#!/bin/bash
 sudo apt-get install python-simplejson python-qt4 python-twisted-conch automake autoconf gcc uml-utilities libtool build-essential pkg-config
 sudo apt-get install libssl-dev iproute tcpdump linux-headers-4.13.0-041300-generic
